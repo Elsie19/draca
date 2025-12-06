7fb14498b143bc6ccb7b1e38c0993e30d9b6e3bf
Elsie Wenger-Stickel <elsie19@pm.me>
2025-12-06 10:16:33 -0500

    panic when type not eq

--- front/src/main.rs
+++ front/src/main.rs
@@ -2,25 +2,12 @@ use interp::interp_sexpr::eval;
 use lexpr::{Cons, Value, print::Options};
 
 const SEXPRS: &str = r###"
-(defenum! option
-  (list (Some v) None)
-  ; TODO: Add derives
-  (:impl (list
-     ('is-some
-       (lambda (self)
-         (match self
-           ((Some _) true)
-           (None     false))))
-     ('is-none
-       (lambda (self)
-         (match self
-           ((Some _) false)
-           (None     true))))
-     ('unwrap
-       (lambda (self)
-         (match self
-           ((Some x) x)
-           (None (panic! "Tried to unwrap None"))))))))
+(defun option/as-list (self)
+  (if (option? self)
+    (match self
+      [(Some x) '(x)]
+      [(None)   '()])
+    (panic! "option/as-list: expected option, got...")))
 "###
 .trim_ascii();
 
