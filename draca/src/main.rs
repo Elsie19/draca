da29a45ffda0ecfde61b9e1ae9d2bd8e5dab9800
Elisabeth Wenger-Stickel <hwengerstickel@protonmail.com>
2025-12-16 10:59:25 -0600

    readline like promp

--- draca/src/main.rs
+++ draca/src/main.rs
@@ -2,6 +2,8 @@
 
 use std::io::{self, Write};
 
+use rustyline::{Config, DefaultEditor, Editor, error::ReadlineError, history::MemHistory};
+
 use crate::{env::Environment, eval::eval, parser::parse};
 
 mod env;
@@ -9,44 +11,53 @@ mod eval;
 mod parser;
 mod stdlib;
 
-pub fn repl() {
-    let mut math_env = Environment::empty()
+pub fn repl() -> rustyline::Result<()> {
+    let mut env = Environment::empty()
         .macros_plugin()
         .sys_plugin()
         .math_plugin()
         .cmp_plugin()
         .build();
 
+    let mut rl = Editor::<(), _>::with_history(Config::builder().build(), MemHistory::new())?;
+
     loop {
-        print!("draca-math> ");
-        io::stdout().flush().unwrap();
-        let expr = read_input().unwrap();
+        let readline = rl.readline("\\> ");
+        match readline {
+            Ok(line) => {
+                let parsed_list = match parse(line.trim()) {
+                    Ok(val) => val,
+                    Err(e) => {
+                        eprintln!("{e}");
+                        std::process::exit(1);
+                    }
+                };
 
-        let parsed_list = match parse(expr.trim()) {
-            Ok(val) => val,
-            Err(e) => {
-                eprintln!("{e}");
-                std::process::exit(1);
+                for expr in parsed_list {
+                    match eval(expr, &mut env) {
+                        Ok(val) => println!(" ==> Ok: {val}"),
+                        Err(e) => eprintln!(" ==> Error: {e}"),
+                    }
+                }
+                rl.add_history_entry(&line)?;
             }
-        };
-
-        for expr in parsed_list {
-            match eval(expr, &mut math_env) {
-                Ok(val) => println!(" ==> Ok: {val}"),
-                Err(e) => eprintln!(" ==> Error: {e}"),
+            Err(ReadlineError::Interrupted) => {
+                println!("^C");
+            }
+            Err(ReadlineError::Eof) => {
+                println!("^D");
+                break;
+            }
+            Err(err) => {
+                eprintln!("{err:?}");
+                break;
             }
         }
     }
+
+    Ok(())
 }
 
-fn read_input() -> io::Result<String> {
-    let mut raw_input = String::new();
-
-    io::stdin().read_line(&mut raw_input)?;
-
-    Ok(raw_input)
-}
-
-fn main() {
-    repl();
+fn main() -> rustyline::Result<()> {
+    repl()
 }
